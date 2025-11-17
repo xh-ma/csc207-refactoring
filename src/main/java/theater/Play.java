82d6de97ef8d0981fc4aedc80b1ca04814c7c29a
Xiaohan M. <xiaohan.m9@gmail.com>
2025-11-17 18:37:50 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,27 @@
 package theater;
 
+/**
+ * Class representing a play with a name and a type.
+ * <p>
+ * The type of the play can be used to determine its genre,
+ * such as "tragedy", "comedy", or others.
+ * </p>
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
     }
 }
